44ead47e6ef937df64c58b2369ccf3bed23115c2
Collin <collinbutrlakorn@gmail.com>
2026-09-06 15:22:10 -0800

    Update db.js

--- rp/js/db.js
+++ rp/js/db.js
@@ -7,4 +7,12 @@ db.version(2).stores({
     teams: 'school, conference, apRank',
     players: 'id, name, school, class, pos, rating, isRecruit',
     recruits: 'id, name, pos, rating, school'
+    fetch('./data/players.json')
+  .then(response => response.json())
+  .then(data => {
+    console.log("Data loaded successfully:", data);
+    // Initialize or feed data to engine.js / db.js
+  })
+  .catch(error => console.error("Error loading JSON:", error));
+
 });
